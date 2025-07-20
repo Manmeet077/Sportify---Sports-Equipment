61eb93616c1c754e614a29d3d545a4eae08b485d
Manmeet077 <singhmanmeet524@gmail.com>
2025-07-20 21:14:06 +0530

    Delete Sportify backend directory

--- Sportify backend/src/main/java/com/sportifyx/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sportifyx.repository;
-
-import com.sportifyx.model.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<OrderItem, Long> {
-}
--- Sportify backend/src/main/java/com/sportifyx/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sportifyx.repository;
-
-import com.sportifyx.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-  Product findByName(String name);
-  void deleteByName(String name);
-}
